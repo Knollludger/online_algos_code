e3e7c2f1bd10b2c341084fd2016944f5be0e5547
Sean Timm <seantimm3@aol.com>
2021-02-04 22:42:39 -0700

    add code

---
+++ Code_For_Class_Questions/rendezvous.jl
@@ -0,0 +1,49 @@
+using Plots
+
+function random_algo(N)
+    index = 1;
+    while(random_flip(N) != random_flip(N))
+        index += 1
+    end
+    return index
+end
+
+function algo_wrapper(N,times, fn)
+    count = 0;
+    index = 0
+    while index < times
+        count += fn(N)
+        index += 1
+    end
+    print("progress: $N   \r")
+    flush(stdout)
+    return (count / times)
+end
+
+function stay_put_algo(N)
+    index = 1;
+    loc = get_rand_location(N)
+    while(index != loc)
+        index += 1
+    end
+    return index
+end
+
+function random_flip(N)
+    return rand(1:N)
+end
+
+function get_rand_location(N)
+    return rand(1:N)
+end
+
+ns = LinRange(1,100000,100000)
+
+println(ns)
+times = 1
+# randoms = map((x) -> algo_wrapper(x,times,random_algo),ns)
+println("\n")
+stay_puts = map((x) -> algo_wrapper(x,times,stay_put_algo),ns)
+
+p = scatter(ns, stay_put_algo,color = "green",xlabel = "number of shops", ylabel = "days taken",bg = RGB(0.2, 0.2, 0.2), smooth=:true)
+# plot!(p,ns,stay_put_algo)
